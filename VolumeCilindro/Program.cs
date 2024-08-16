90db105ac182ab25a2b4d6683896dbb0335485b7
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-08-16 11:53:03 -0300

    volume cilindro

---
+++ VolumeCilindro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VolumeCilindro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			double R, A, h, V;
+
+			R = 0;
+			while (R <= 0)
+			{
+                Console.Write("Digite o valor do raio da base do cilindro: ");
+				R = double.Parse(Console.ReadLine());
+            }
+
+			h = 0;
+			while (h <= 0)
+			{
+				Console.Write("Digite o valor do raio da altura do cilindro: ");
+				h = double.Parse(Console.ReadLine()); ;
+            }
+
+            Console.WriteLine();
+
+			//área da base
+			A = Math.PI * Math.Pow(R, 2);
+			//volume do cilindro
+			V = A * h;
+            Console.WriteLine($"Volume do cilindro {V}");
+        }
+	}
+}
